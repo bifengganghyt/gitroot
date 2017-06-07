6eb414ffa26cb9b3c89c44cb2a038058fc4ce6e2
heyutong <418239445@qq.com>
2017-06-07 17:31:34 +0800

    spring

---
+++ javaweb/【37】Spring/code/day37Spring01_IOC/src/cn/itcast/b_create_obj/App.java
@@ -0,0 +1,32 @@
+package cn.itcast.b_create_obj;
+
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	@Test
+	public void test() {
+		// 方式1：创建容器对象
+		/*ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("cn/itcast/b_create_obj/bean.xml");//打印:User.User(), 创建User对象...
+		User user = (User) ac.getBean("user1");
+		System.out.println(user);*/
+		
+		// 方式2： 会从当前类所在的包下找bean.xml  【测试方便】
+		/*ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml", this.getClass());//打印:User.User(), 创建User对象...
+		String str = (String)ac.getBean("str");
+		System.out.println(str);//打印:Jack*/
+		
+		// 方式2： 会从当前类所在的包下找bean.xml  【测试方便】
+		/*ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean.xml", this.getClass());//打印:User.User(), 创建User对象...
+		User user = (User) ac.getBean("user");
+		System.out.println(user);//打印:User [id=10000, name=Jack]*/	
+
+		//方式2： 会从当前类所在的包下找bean.xml  【测试方便】
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("bean2.xml", this.getClass());
+	
+	
+	
+	
+	}
+}
