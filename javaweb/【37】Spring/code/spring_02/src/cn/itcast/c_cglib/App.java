8064891e9b62eee08e902ec84ea50b27d96f2499
heyutong <418239445@qq.com>
2017-06-12 23:35:27 +0800

    spring

--- javaweb/【37】Spring/code/spring_02/src/cn/itcast/c_cglib/App.java
+++ javaweb/【37】Spring/code/spring_02/src/cn/itcast/c_cglib/App.java
@@ -4,13 +4,13 @@ public class App {
 	public static void main(String[] args) {
 		// 创建目标对象
 		UserDao target = new UserDao();
-		System.out.println("目标对象：" + target.getClass());  // class cn.itcast.c_cglib.UserDao
+		System.out.println("目标对象：" + target.getClass());  //目标对象：class cn.itcast.c_cglib.UserDao
 		
 		// 代理对象
 		UserDao proxy = (UserDao) new ProxyFactory(target).getProxyInstance();
-		System.out.println("代理对象： " + proxy.getClass());  //  class cn.itcast.c_cglib.UserDao$$EnhancerByCGLIB$$6ecf51fe
+		System.out.println("代理对象： " + proxy.getClass());  //代理对象： class cn.itcast.c_cglib.UserDao$$EnhancerByCGLIB$$f0dc6d19
 		
 		// 执行代理对象的方法
-		proxy.save();
+		proxy.save();  //打印:模拟： 保存用户！
 	}
 }
