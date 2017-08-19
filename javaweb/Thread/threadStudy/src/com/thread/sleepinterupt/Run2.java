8ac6e53a47d1e97d412e96d983ffedbeb49a4d5e
heyutong <418239445@qq.com>
2017-08-19 17:38:36 +0800

    linux

--- javaweb/Thread/threadStudy/src/com/thread/sleepinterupt/Run2.java
+++ javaweb/Thread/threadStudy/src/com/thread/sleepinterupt/Run2.java
@@ -1,8 +1,18 @@
 package com.thread.sleepinterupt;
 
+import java.awt.List;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PipedReader;
+import java.io.PipedWriter;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.omg.CORBA.PRIVATE_MEMBER;
+
 public class Run2 {
 	public static void main(String[] args) {
-		MyThrrad myThrrad = new MyThrrad();
+		/*MyThrrad myThrrad = new MyThrrad();
 		myThrrad.setDaemon(true);
 		myThrrad.start();
 		try {
@@ -11,6 +21,11 @@ public class Run2 {
 			e.printStackTrace();
 		}
 		
-		myThrrad.interrupt();
+		myThrrad.interrupt();*/
+		String a = "a";
+		String b = "a";
+		System.out.println(a==b);
+   
+		
 	}
 }
